586bb868129f1cd3572d0784195f780204c47763
abbyd <abbydiaz300@gmail.com>
2023-10-07 21:21:24 -0700

    got response to show on page, need to format

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,70 @@
+var cityFormEl = document.querySelector('#city-form');
+var cityInputEl = document.querySelector('#enterCity');
+var weatherContainerEl = document.querySelector('#weather-container');
+var weatherSearchTerm = document.querySelector('#weather-search-term');
+
 var getWeather = function(name) {
 
     // format weather api url
-    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + name + '&APPID=28d6a72429cf685ec90ed4979e8a1345&units=imperial';
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + name + '&APPID=28d6a72429cf685ec90ed4979e8a1345&units=imperial&main=main.temp';
 
     // make request to url
    fetch(apiUrl).then(function(response) {
     response.json().then(function(data) {
-        console.log(data);
+        displayWeather(data, name);
     });
    });
 };
 
-getWeather('Oxnard');
+var formSubmitHandler = function(event) {
+
+    event.preventDefault();
+
+    // get value from input element 
+    var city = cityInputEl.value.trim();
+    if(city) {
+        getWeather(city);
+        cityInputEl.value = '';
+    } else {
+        alert('Enter a city you dingus');
+    }
+  
+};
+
+var displayWeather = function (weather, searchTerm) {
+    console.log(weather);
+    console.log(searchTerm);
+
+    // clear old content
+    weatherContainerEl.textContent = '';
+    weatherSearchTerm.textContent = searchTerm;
+
+    // loop over info
+    
+
+        // format weather info
+        var weatherStuff = JSON.stringify(weather);
+
+        // create container for weather info
+        var info = document.createElement('div');
+        info.classList = "justify-space-between align-center";
+
+        // create span to hold weather 
+        var titleEl = document.createElement('span');
+        titleEl.textContent = weatherStuff;
+
+        // append to container
+        info.appendChild(titleEl);
+
+        // append container to DOM
+        weatherContainerEl.appendChild(info);
+     
+
+    
+};
+
+displayWeather();
+
+cityFormEl.addEventListener('submit', formSubmitHandler);
+
+//getWeather('Oxnard');
