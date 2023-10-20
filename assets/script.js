8e8f02d2c49469f96e0f027702bad52218d69de8
abbyd <abbydiaz300@gmail.com>
2023-10-19 19:48:52 -0700

    finally figured out how to acess api info

--- assets/script.js
+++ assets/script.js
@@ -6,15 +6,38 @@ var weatherSearchTerm = document.querySelector('#weather-search-term');
 var getWeather = function(name) {
 
     // format weather api url
-    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + name + '&APPID=28d6a72429cf685ec90ed4979e8a1345&units=imperial&main=main.temp';
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + name + '&APPID=28d6a72429cf685ec90ed4979e8a1345&units=imperial';
 
-    // make request to url
-   fetch(apiUrl).then(function(response) {
-    response.json().then(function(data) {
+//     // make request to url
+//    fetch(apiUrl).then(function(response) {
+//     response.json().then(function(data) {
+//         displayWeather(data, name);
+//     });
+//    });
+fetch(apiUrl)
+.then(function(response) {
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+    return response.json();
+})
+.then(function(data) {
+    // Check if the 'coord' property exists in the response
+    if (data && data.coord) {
         displayWeather(data, name);
-    });
-   });
+    } else {
+        throw new Error('Invalid data format received from the API');
+    }
+})
+.catch(function(error) {
+    console.error('Error:', error);
+    // Handle the error, show a user-friendly message, etc.
+});
+
+
 };
+
+
 
 var formSubmitHandler = function(event) {
 
@@ -31,37 +54,38 @@ var formSubmitHandler = function(event) {
   
 };
 
-var displayWeather = function (weather, searchTerm) {
-    console.log(weather);
+var displayWeather = function (weatherData, searchTerm,) {
+
+    console.log(weatherData);
     console.log(searchTerm);
 
     // clear old content
     weatherContainerEl.textContent = '';
     weatherSearchTerm.textContent = searchTerm;
-
-    // loop over info
+    
     
 
-        // format weather info
-        var weatherStuff = JSON.stringify(weather);
+        // Check if weatherData contains 'main' property
+        if (weatherData && weatherData.main) {
+            var temperature = weatherData.main.temp;
+            var temperatureDiv = document.createElement('div');
+            temperatureDiv.textContent = 'Temperature: ' + temperature + '°F';
+            weatherContainerEl.appendChild(temperatureDiv);
 
-        // create container for weather info
-        var info = document.createElement('div');
-        info.classList = "justify-space-between align-center";
-
-        // create span to hold weather 
-        var titleEl = document.createElement('span');
-        titleEl.textContent = weatherStuff;
-
-        // append to container
-        info.appendChild(titleEl);
-
-        // append container to DOM
-        weatherContainerEl.appendChild(info);
-     
-
+            var feelsLike = weatherData.main.feels_like;
+            var feelsLikeDiv = document.createElement('div');
+            feelsLikeDiv.textContent = 'Feels Like: ' + feelsLike + '°F';
+            weatherContainerEl.appendChild(feelsLikeDiv);
+        } else {
+            // Handle the case where 'main' property is not available in the response
+            var errorDiv = document.createElement('div');
+            errorDiv.textContent = 'Weather data not available for the specified location.';
+            weatherContainerEl.appendChild(errorDiv);
+        }
     
 };
+
+
 
 displayWeather();
 
